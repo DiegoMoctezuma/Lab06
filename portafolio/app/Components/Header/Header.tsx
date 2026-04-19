e121a878772a480a93deeb24f8944b342ec87de1
DiegoMoctezuma <moctezumarafael26@gmail.com>
2026-04-18 22:09:49 -0600

    Correción de estilos faltantes en header

--- portafolio/app/Components/Header/Header.tsx
+++ portafolio/app/Components/Header/Header.tsx
@@ -8,44 +8,44 @@ export default function Header() {
     return (
         <header className={styles.header} id="header">
             <nav className={`container ${styles.nav}`}>
-                <Link href="#home" className="nav__logo">
-                    <span className="logo__bracket">&lt;</span>{headerData}<span className="logo__bracket">/&gt;</span>
+                <Link href="#home" className={styles.nav__logo}>
+                    <span className={styles.logo__bracket}>&lt;</span>{headerData}<span className={styles.logo__bracket}>/&gt;</span>
                 </Link>
 
-                <input type="checkbox" id="nav-toggle" className="nav__checkbox" />
-                <label htmlFor="nav-toggle" className="nav__toggle">
+                <input type="checkbox" id="nav-toggle" className={styles.nav__checkbox} />
+                <label htmlFor="nav-toggle" className={styles.nav__toggle}>
                     <i className="fas fa-bars"></i>
                 </label>
 
-                <div className="nav__menu" id="nav-menu">
-                    <label htmlFor="nav-toggle" className="nav__close">
+                <div className={styles.nav__menu} id="nav-menu">
+                    <label htmlFor="nav-toggle" className={styles.nav__close}>
                         <i className="fas fa-times"></i>
                     </label>
 
-                    <ul className="nav__list">
-                        <li className="nav__item">
+                    <ul className={styles.nav__list}>
+                        <li className={styles.nav__item}>
                             <label htmlFor="nav-toggle">
-                                <Link href="#home" className="nav__link">Inicio</Link>
+                                <Link href="#home" className={styles.nav__link}>Inicio</Link>
                             </label>
                         </li>
-                        <li className="nav__item">
+                        <li className={styles.nav__item}>
                             <label htmlFor="nav-toggle">
-                                <Link href="#interests" className="nav__link">Intereses</Link>
+                                <Link href="#interests" className={styles.nav__link}>Intereses</Link>
                             </label>
                         </li>
-                        <li className="nav__item">
+                        <li className={styles.nav__item}>
                             <label htmlFor="nav-toggle">
-                                <Link href="#cv" className="nav__link">CV</Link>
+                                <Link href="#cv" className={styles.nav__link}>CV</Link>
                             </label>
                         </li>
-                        <li className="nav__item">
+                        <li className={styles.nav__item}>
                             <label htmlFor="nav-toggle">
-                                <Link href="#projects" className="nav__link">Proyectos</Link>
+                                <Link href="#projects" className={styles.nav__link}>Proyectos</Link>
                             </label>
                         </li>
-                        <li className="nav__item">
+                        <li className={styles.nav__item}>
                             <label htmlFor="nav-toggle">
-                                <Link href="#contact" className="nav__link">Contacto</Link>
+                                <Link href="#contact" className={styles.nav__link}>Contacto</Link>
                             </label>
                         </li>
                     </ul>
